a7e35cd4283dd4f375885c851e56aa49b790e139
lelegoyeng <lelegoyengs@gmail.com>
2023-09-20 11:50:09 +0700

    update

--- pages/strategy.js
+++ pages/strategy.js
@@ -1,9 +1,23 @@
 import { useEffect, useState } from 'react';
 const axios = require('axios');
-const config = require('../config/index')
+const config = require('../config/index');
+
 
 export default function Strategy() {
+    const [dataGET, setDataGet] = useState(false);
 
+    const fetchData = async () => {
+        try {
+            const response = await axios.get('http://localhost:4000/candles', config.configHeader);
+            setCandles(response?.data?.result);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [dataGET]);
 
     return (
         <>
