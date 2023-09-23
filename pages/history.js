04838bec37910e6f6f8e6289a1c3b2bc431cb283
lelegoyeng <lelegoyengs@gmail.com>
2023-09-23 16:59:37 +0700

    update

--- pages/history.js
+++ pages/history.js
@@ -40,59 +40,52 @@ export default function History() {
             </div>
             <div className="card w-full bg-base-100 shadow-xl mt-5">
                 <h2 className="card-title text-gray-700 text-2xl font-bold ml-10 mt-5">Transaction Of The Month</h2>
-                <div className="card-body flex px-36 justify-center items-center">
-                    {/* <Line
-                        data={dataChartWeekly}
-                        options={optionsChartWeekly}
-                    /> */}
-                    <div className='text-gray-700 text-md font-bold mb-5 mt-5'>Weekly Profit: {history?.result?.totalWeekly.toFixed(2)}</div>
-                </div>
-            </div>
-
-            <div className="card w-full bg-base-100 shadow-xl mt-5">
-                <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
-                    </div>
-                </div>
+                <LineChart
+                    title={`This Month`}
+                    labels={['Week 1', 'Week 2', 'Week 3', 'Week 4']}
+                    data={[
+                        history?.result?.oneMonth?.week1,
+                        history?.result?.oneMonth?.week2,
+                        history?.result?.oneMonth?.week3,
+                        history?.result?.oneMonth?.week4,
+                    ]}
+                    profit={history?.result?.totalOneMonth.toFixed(2)}
+                />
             </div>
             <div className="card w-full bg-base-100 shadow-xl mt-5">
-                <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
-                    </div>
-                </div>
-            </div>
-            <div className="card w-full bg-base-100 shadow-xl mt-5">
-                <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
-                    </div>
-                </div>
-            </div>
-            <div className="card w-full bg-base-100 shadow-xl mt-5">
-                <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
-                    </div>
-                </div>
-            </div>
-            <div className="card w-full bg-base-100 shadow-xl mt-5">
-                <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
-                    </div>
-                </div>
+                <h2 className="card-title text-gray-700 text-2xl font-bold ml-10 mt-5">Transaction Of The Year</h2>
+                <LineChart
+                    title={`This Year`}
+                    labels={[
+                        'January',
+                        'February',
+                        'March',
+                        'April',
+                        'May',
+                        'June',
+                        'July',
+                        'August',
+                        'September',
+                        'October',
+                        'November',
+                        'December',
+                    ]}
+                    data={[
+                        history?.result?.Year?.january,
+                        history?.result?.Year?.february,
+                        history?.result?.Year?.march,
+                        history?.result?.Year?.april,
+                        history?.result?.Year?.may,
+                        history?.result?.Year?.june,
+                        history?.result?.Year?.july,
+                        history?.result?.Year?.august,
+                        history?.result?.Year?.september,
+                        history?.result?.Year?.october,
+                        history?.result?.Year?.november,
+                        history?.result?.Year?.december,
+                    ]}
+                    profit={history?.result?.totalYear.toFixed(2)}
+                />
             </div>
         </div>
     );
